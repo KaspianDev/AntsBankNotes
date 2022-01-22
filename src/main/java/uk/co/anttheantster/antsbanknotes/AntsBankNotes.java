175a7580ffc7eb75b8f9ade6a1e0ae18c70bfd2e
AntTheAntster <anttheantster@gmail.com>
2022-01-22 16:10:38 +0000

    Made item and withdraw command
    Made item and withdraw command

--- src/main/java/uk/co/anttheantster/antsbanknotes/AntsBankNotes.java
+++ src/main/java/uk/co/anttheantster/antsbanknotes/AntsBankNotes.java
@@ -1,14 +1,26 @@
 package uk.co.anttheantster.antsbanknotes;
 
+import net.milkbowl.vault.economy.Economy;
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
+import uk.co.anttheantster.antsbanknotes.commands.WithdrawCommand;
 
 public class AntsBankNotes extends JavaPlugin {
 
     private static AntsBankNotes instance;
 
+    private static Economy econ = null;
+
     @Override
     public void onEnable() {
         instance = this;
+
+        if (!setupEconomy() ) {
+            getLogger().severe(String.format("[%s] - Disabled due to no Vault dependency found!", getDescription().getName()));
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
+
         setup();
     }
 
@@ -25,6 +37,22 @@ public class AntsBankNotes extends JavaPlugin {
     }
 
     private void registerCommandsAndListeners(){
+        getCommand("withdraw").setExecutor(new WithdrawCommand());
+    }
 
+    private boolean setupEconomy() {
+        if (getServer().getPluginManager().getPlugin("Vault") == null) {
+            return false;
+        }
+        RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
+        if (rsp == null) {
+            return false;
+        }
+        econ = rsp.getProvider();
+        return econ != null;
+    }
+
+    public static Economy getEconomy(){
+        return econ;
     }
 }
--- src/main/java/uk/co/anttheantster/antsbanknotes/commands/WithdrawCommand.java
+++ src/main/java/uk/co/anttheantster/antsbanknotes/commands/WithdrawCommand.java
@@ -1,15 +1,28 @@
 package uk.co.anttheantster.antsbanknotes.commands;
 
+import com.sun.istack.internal.NotNull;
+import net.milkbowl.vault.economy.Economy;
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemFlag;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import uk.co.anttheantster.antsbanknotes.AntsBankNotes;
+
+import java.util.ArrayList;
+import java.util.Objects;
 
 public class WithdrawCommand implements CommandExecutor {
 
     private AntsBankNotes antsBankNotes = AntsBankNotes.getInstance();
+    private FileConfiguration config = antsBankNotes.getConfig();
+    private Economy econ = AntsBankNotes.getEconomy();
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
@@ -34,6 +47,51 @@ public class WithdrawCommand implements CommandExecutor {
             if (args.length > 1){
                 player.sendMessage(ChatColor.RED + "Usage: /withdraw <Amount>");
                 return false;
+            }
+            int amount = 0;
+            try {
+                amount = Integer.parseInt(args[0]);
+
+            } catch (Exception e){
+                player.sendMessage(ChatColor.RED + "Incorrect Usage!");
+                player.sendMessage(ChatColor.RED + "Usage: /withdraw <Amount>");
+                return false;
+            }
+            if (amount > econ.getBalance(player)){
+                player.sendMessage(ChatColor.RED + "Insufficient Funds!");
+                return false;
+            }
+            econ.withdrawPlayer(player, amount);
+
+
+            ItemStack moneyItem = null;
+            ItemMeta moneyItemMeta = null;
+            try {
+                moneyItem = new ItemStack(Objects.requireNonNull(Material.getMaterial(config.getString("Money Item"))));
+                moneyItemMeta = moneyItem.getItemMeta();
+            } catch (Exception e){
+                player.sendMessage(ChatColor.RED + "Invalid Item. Please ensure the 'Money Item' in the config is correct");
+                return false;
+            }
+
+            ArrayList<String> lore = new ArrayList<String>();
+            lore.add(ChatColor.AQUA + "Amount: " + ChatColor.GOLD + amount);
+            lore.add(ChatColor.AQUA + "Signed: " + ChatColor.GOLD + player.getName());
+
+            moneyItemMeta.setDisplayName(ChatColor.GOLD + "Banknote of: " + ChatColor.GREEN + amount);
+            moneyItemMeta.setLore(lore);
+            moneyItemMeta.setUnbreakable(true);
+            moneyItemMeta.addEnchant(Enchantment.DAMAGE_ALL, 1, true);
+            moneyItemMeta.addItemFlags(ItemFlag.HIDE_ATTRIBUTES);
+            moneyItemMeta.addItemFlags(ItemFlag.HIDE_UNBREAKABLE);
+            moneyItem.setItemMeta(moneyItemMeta);
+
+            try {
+                player.getInventory().addItem(moneyItem);
+                player.sendMessage(ChatColor.GOLD + "You successfully withdrew:" + ChatColor.GREEN + amount);
+            } catch (Exception e){
+                player.sendMessage(ChatColor.RED + "No Inventory Space!");
+                econ.depositPlayer(player, amount);
             }
         }
 
