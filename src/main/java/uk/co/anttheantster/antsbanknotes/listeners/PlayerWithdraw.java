8f17ec6371808a06d0849298338ec1de671b5e64
Kaspian <34658474+KaspianDev@users.noreply.github.com>
2022-08-06 16:38:44 +0000

    stuff

--- src/main/java/uk/co/anttheantster/antsbanknotes/listeners/PlayerWithdraw.java
+++ src/main/java/uk/co/anttheantster/antsbanknotes/listeners/PlayerWithdraw.java
@@ -4,8 +4,8 @@ import net.milkbowl.vault.economy.Economy;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
+import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEvent;
 import uk.co.anttheantster.antsbanknotes.AntsBankNotes;
 import uk.co.anttheantster.antsbanknotes.commands.WithdrawCommand;
@@ -17,12 +17,12 @@ public class PlayerWithdraw extends WithdrawCommand implements Listener {
     public void redeemBanknote(PlayerInteractEvent e){
         Economy econ = AntsBankNotes.getEconomy();
 
-        if (e.hasItem()){
+        if (e.hasItem() && e.getAction().equals(Action.RIGHT_CLICK_AIR)){
             Player p = e.getPlayer();
             if (p.getInventory().getItemInMainHand().equals(CreateBankNote.bankNote)){
                 econ.depositPlayer(p, CreateBankNote.noteAmount);
                 p.getInventory().setItemInMainHand(null);
-                p.sendMessage(ChatColor.GOLD + "You have successfully deposited: " + ChatColor.GREEN + CreateBankNote.noteAmount);
+                p.sendMessage(ChatColor.GOLD + "Wpłaciłeś/aś: " + ChatColor.GREEN + CreateBankNote.noteAmount + "$");
                 e.setCancelled(true);
             }
         }
